6db806ee1da184fe748656381d9992e11d118c3a
DarkEyeDragon <joshua.tack@student.vives.be>
2020-06-18 17:48:54 +0200

    Remove debug message and added color to info message

--- src/main/java/me/darkeyedragon/randomtp/listener/PluginLoadListener.java
+++ src/main/java/me/darkeyedragon/randomtp/listener/PluginLoadListener.java
@@ -2,6 +2,7 @@ package me.darkeyedragon.randomtp.listener;
 
 import me.darkeyedragon.randomtp.RandomTeleport;
 import me.darkeyedragon.randomtp.validator.ValidatorFactory;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.PluginEnableEvent;
@@ -16,10 +17,9 @@ public class PluginLoadListener implements Listener {
 
     @EventHandler
     public void onPluginEnable(PluginEnableEvent event) {
-        System.out.println("RTP: " + event.getPlugin().getName() + " loaded");
         plugin.getValidatorList().replaceAll(chunkValidator -> {
             if (chunkValidator.getValidator().getName().equalsIgnoreCase(event.getPlugin().getName()) && !chunkValidator.isLoaded()) {
-                plugin.getLogger().info("Loaded " + event.getPlugin().getName() + " as validator.");
+                plugin.getLogger().info(ChatColor.GREEN + event.getPlugin().getName() + " as validator.");
                 return ValidatorFactory.createFrom(chunkValidator.getValidator());
             } else {
                 return chunkValidator;
