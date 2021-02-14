fe2e6cc6b711b6bdb347cb1dd3dcb5c06d2f3e9f
DarkEyeDragon <joshua.tack@student.vives.be>
2021-02-14 18:28:53 +0100

    Fix scheduler

--- Spigot/src/main/java/me/darkeyedragon/randomtp/scheduler/SpigotScheduler.java
+++ Spigot/src/main/java/me/darkeyedragon/randomtp/scheduler/SpigotScheduler.java
@@ -4,6 +4,7 @@ import me.darkeyedragon.randomtp.RandomTeleport;
 import me.darkeyedragon.randomtp.api.scheduler.Scheduler;
 import me.darkeyedragon.randomtp.api.scheduler.Task;
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.function.Consumer;
@@ -18,41 +19,26 @@ public class SpigotScheduler implements Scheduler {
 
     @Override
     public Task runTaskTimer(Runnable runnable, long delay, long interval) {
-        BukkitTask task = instance.getPlugin().getServer().getScheduler().runTaskTimer(instance.getPlugin(), runnable, delay, interval);
+        BukkitTask task = Bukkit.getScheduler().runTaskTimer(instance.getPlugin(), runnable, delay, interval);
         return toTask(task);
     }
 
     @Override
     public Task runTaskLater(Runnable runnable, long delay) {
         BukkitTask task = Bukkit.getScheduler().runTaskLater(instance.getPlugin(), runnable, delay);
-        return new Task() {
-            @Override
-            public void cancel() {
-                task.cancel();
-            }
-
-            @Override
-            public int getTaskId() {
-                return task.getTaskId();
-            }
-
-            @Override
-            public boolean isCancelled() {
-                return task.isCancelled();
-            }
-        };
+        return toTask(task);
     }
 
-    //TODO Trigary fix pl0x
     @Override
     public void runTaskTimer(Consumer<Task> taskConsumer, long delay, long interval) {
-        Runnable runnable = new Runnable() {
+        new BukkitRunnable() {
+            private final Task task = toTask(this);
+
             @Override
             public void run() {
-
+                taskConsumer.accept(task);
             }
-        };
-        Bukkit.getScheduler().runTaskTimer(instance.getPlugin(), runnable, delay, interval);
+        }.runTaskTimer(instance.getPlugin(), delay, interval);
     }
 
     private Task toTask(BukkitTask bukkitTask) {
@@ -70,6 +56,25 @@ public class SpigotScheduler implements Scheduler {
             @Override
             public boolean isCancelled() {
                 return bukkitTask.isCancelled();
+            }
+        };
+    }
+
+    private Task toTask(BukkitRunnable bukkitRunnable) { //TODO ded be happy
+        return new Task() {
+            @Override
+            public void cancel() {
+                bukkitRunnable.cancel();
+            }
+
+            @Override
+            public int getTaskId() {
+                return bukkitRunnable.getTaskId();
+            }
+
+            @Override
+            public boolean isCancelled() {
+                return bukkitRunnable.isCancelled();
             }
         };
     }
